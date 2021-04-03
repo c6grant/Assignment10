7142e06c9cd54e664cf3fc922010da9d6666db29
Connor Grant <71059606+c6grant@users.noreply.github.com>
2021-04-03 11:51:39 -0600

    Add project files.

---
+++ Assignment10/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+
+        //Index View Model to filter down to Bowlers, help w/ PageNumbInfo, and Team Name
+        
+        public List<Bowlers> Bowlers { get; set; }
+
+        public PageNumberingInfo PageNumberingInfo { get; set; }
+
+        public string Team { get; set; }
+
+
+    }
+}
